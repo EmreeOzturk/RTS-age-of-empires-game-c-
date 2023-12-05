cf1ec4dbb58531a480af71791b46b3fb2d0386c2
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-06 02:07:32 +0300

    added select mechanics

---
+++ Mechanics.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mechanics : MonoBehaviour
+{
+    public Material normalMaterial;
+    public Material choosenMaterial;
+    public GameObject oldObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.gameObject.TryGetComponent<MeshRenderer>(out var obj) && obj.material != choosenMaterial && obj.gameObject.layer != 6)
+                {
+                    if (oldObject != null)
+                    {
+                        oldObject.GetComponent<MeshRenderer>().material = normalMaterial;
+                    }
+                    obj.material = choosenMaterial;
+                    oldObject = hit.transform.gameObject;
+                }
+                Debug.Log(hit.transform.gameObject.name);
+            }
+        }
+    }
+}
