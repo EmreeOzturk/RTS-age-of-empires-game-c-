62a81b6ba97e2a6b07891b0886ceaaeeedd1522c
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-16 02:29:15 +0300

    added spawn unit

--- Building.cs
+++ Building.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject spawnUnit;
+    Vector3 spawnPoint;
+    float spawnTime;
+    float spawnTimer;
+    bool isSpawning;
+
+
     void Start()
     {
-        
+        BuildingSelection.Instance.allBuildingsInGame.Add(this.gameObject);
+        spawnPoint = this.transform.position + new Vector3(0, 0, 5);
+        spawnTime = 3;
+        spawnTimer = spawnTime;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isSpawning && spawnUnit != null)
+        {
+            spawnTimer -= Time.deltaTime;
+            if (spawnTimer <= 0)
+            {
+                GameObject unit = Instantiate(spawnUnit, spawnPoint, Quaternion.identity);
+                unit.GetComponent<Uunit>().SetNavMesh();
+                unit.GetComponent<Uunit>().MoveUnit(spawnPoint + new Vector3(0, 0, 0.1f));
+                spawnTimer = spawnTime;
+                spawnPoint += new Vector3(0, 0, 1);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            isSpawning = !isSpawning;
+            spawnPoint = this.transform.position + new Vector3(0, 0, 5);
+        }
+
     }
+    void OnDestroy()
+    {
+        BuildingSelection.Instance.allBuildingsInGame.Remove(this.gameObject);
+    }
+
 }
