1c4424d1832ee5b8ee4a57266ac02a0ebcc68bd6
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-16 11:22:02 +0300

    ..

--- Building.cs
+++ Building.cs
@@ -4,41 +4,88 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
+
     public GameObject spawnUnit;
-    Vector3 spawnPoint;
+    public Vector3 spawnPoint;
+
+
     float spawnTime;
     float spawnTimer;
     bool isSpawning;
 
 
+    public enum BuildingType
+    {
+        House,
+        Barracks,
+        ResourceCollector
+    }
+
+    public BuildingType buildingType;
+
+
     void Start()
     {
+
         BuildingSelection.Instance.allBuildingsInGame.Add(this.gameObject);
-        spawnPoint = this.transform.position + new Vector3(0, 0, 5);
-        spawnTime = 3;
-        spawnTimer = spawnTime;
+        if (buildingType == BuildingType.House)
+        {
+            isSpawning = false;
+            // spawnUnit = Resources.Load("Prefabs/Units/Collector") as Prefa;
+            spawnTime = 3;
+            spawnTimer = spawnTime;
+            spawnPoint = this.transform.position + new Vector3(0, 0, 5);
+
+        }
+        else if (buildingType == BuildingType.Barracks)
+        {
+            isSpawning = false;
+            // spawnUnit = Resources.Load("Prefabs/Units/Soldier") as GameObject;
+            spawnTime = 5;
+            spawnTimer = spawnTime;
+            spawnPoint = this.transform.position + new Vector3(0, 0, 5);
+
+        }
+        else if (buildingType == BuildingType.ResourceCollector)
+        {
+            
+            isSpawning = false;
+            spawnUnit = null;
+        }
+
     }
     void Update()
     {
-        if (isSpawning && spawnUnit != null)
+        if (Input.GetKeyDown(KeyCode.S) && (buildingType == BuildingType.House || buildingType == BuildingType.Barracks))
         {
-            spawnTimer -= Time.deltaTime;
-            if (spawnTimer <= 0)
-            {
-                GameObject unit = Instantiate(spawnUnit, spawnPoint, Quaternion.identity);
-                unit.GetComponent<Uunit>().SetNavMesh();
-                unit.GetComponent<Uunit>().MoveUnit(spawnPoint + new Vector3(0, 0, 0.1f));
-                spawnTimer = spawnTime;
-                spawnPoint += new Vector3(0, 0, 1);
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
+            Debug.Log("Spawning SSSS");
             isSpawning = !isSpawning;
+            Debug.Log("isSpawning: " + isSpawning);
             spawnPoint = this.transform.position + new Vector3(0, 0, 5);
         }
 
+        if (isSpawning && (buildingType == BuildingType.House || buildingType == BuildingType.Barracks))
+        {
+            Debug.Log("Girdi mi ?");
+            spawnTimer -= Time.deltaTime;
+            if (spawnTimer <= 0)
+            {
+                SpawnUnit();
+            }
+        }
+
+
+
+    }
+
+    public void SpawnUnit()
+    {
+        Debug.Log("Spawn unit");
+        GameObject unit = Instantiate(spawnUnit, spawnPoint, Quaternion.identity);
+        unit.GetComponent<Uunit>().SetNavMesh();
+        unit.GetComponent<Uunit>().MoveUnit(spawnPoint + new Vector3(0, 0, 0.1f));
+        spawnTimer = spawnTime;
+        spawnPoint += new Vector3(0, 0, 1);
     }
     void OnDestroy()
     {
