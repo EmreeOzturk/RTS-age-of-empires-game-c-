4dc249da820435cf39966b8bb73bae3adac6b9dd
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-23 11:55:18 +0300

    Add resources, unit panel, and unit spawning functionality

--- Game.cs
+++ Game.cs
@@ -1,23 +1,98 @@
 using System.Collections;
 using System.Collections.Generic;
+using ExitGames.Client.Photon.StructWrapping;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
+    [Header("Resources")]
+    public TextMeshProUGUI stoneText;
+    public TextMeshProUGUI goldText;
+    public TextMeshProUGUI woodText;
+
+    [Header("Unit Panel")]
+    public GameObject unitPanel;
+    public GameObject housePanel;
+    public GameObject barracksPanel;
+
+    public BuildingSelection buildingSelection;
+
+    public GameObject NotEnoughResourcesPanel;
+    public TextMeshProUGUI unitHealthText;
+    public TextMeshProUGUI unitAttackText;
+    public TextMeshProUGUI unitDefenceText;
+    public TextMeshProUGUI unitSpellText;
+    public TextMeshProUGUI unitNameText;
+    public Image image;
+
     public int wood;
     public int stone;
     public int gold;
-    // Start is called before the first frame update
+
+    public GameObject collector;
+    public GameObject soldier;
+
     void Start()
     {
-
+        stoneText.text = stone.ToString();
+        goldText.text = gold.ToString();
+        woodText.text = wood.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator HideMessage()
     {
-
+        yield return new WaitForSeconds(1);
+        NotEnoughResourcesPanel.SetActive(false);
     }
 
-    
+    public void SpawnUnit()
+    {
+        GameObject building = BuildingSelection.Instance.selectedBuildings[0];
+        if (building.GetComponent<Building>().buildingType == Building.BuildingType.Barracks)
+        {
+            if (wood >= 10 && stone >= 10 && gold >= 10)
+            {
+                wood -= 10;
+                stone -= 10;
+                gold -= 10;
+                stoneText.text = stone.ToString();
+                goldText.text = gold.ToString();
+                woodText.text = wood.ToString();
+                GameObject unit = Instantiate(building.GetComponent<Building>().spawnUnit, building.GetComponent<Building>().spawnPoint, Quaternion.identity);
+                unit.GetComponent<Uunit>().SetNavMesh();
+                unit.GetComponent<Uunit>().MoveUnit(building.GetComponent<Building>().spawnPoint + new Vector3(0, 0, 0.1f));
+                building.GetComponent<Building>().spawnPoint += new Vector3(0, 0, 1);
+
+
+            }
+            else
+            {
+                NotEnoughResourcesPanel.SetActive(true);
+                StartCoroutine(HideMessage());
+            }
+        }
+        else if (building.GetComponent<Building>().buildingType == Building.BuildingType.House)
+        {
+            if (wood >= 10 && stone >= 10 && gold >= 10)
+            {
+                wood -= 10;
+                stone -= 10;
+                gold -= 10;
+                stoneText.text = stone.ToString();
+                goldText.text = gold.ToString();
+                woodText.text = wood.ToString();
+                GameObject unit = Instantiate(building.GetComponent<Building>().spawnUnit, building.GetComponent<Building>().spawnPoint, Quaternion.identity);
+                unit.GetComponent<Uunit>().SetNavMesh();
+                unit.GetComponent<Uunit>().MoveUnit(building.GetComponent<Building>().spawnPoint + new Vector3(0, 0, 0.1f));
+                building.GetComponent<Building>().spawnPoint += new Vector3(0, 0, 1);
+            }
+            else
+            {
+                NotEnoughResourcesPanel.SetActive(true);
+                StartCoroutine(HideMessage());
+            }
+        }
+    }
 }
